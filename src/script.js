20eeed8b5b768ac4f6ea3e83c011ce4107baff6e
Eliza <94659901+elizacaws@users.noreply.github.com>
2021-11-28 16:12:23 -0800

    Added form and JS for city search

--- src/script.js
+++ src/script.js
@@ -43,8 +43,19 @@ function displayTemperature(response) {
   );
 }
 
-let apiKey = "6df09177bf10bb0a2b5eb9a9dc845bfd";
-let city = "Sydney";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "6df09177bf10bb0a2b5eb9a9dc845bfd";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Los Angeles");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
