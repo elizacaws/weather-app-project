f7d4e2a35836b3e2d938d17f67377d58f6583a2a
Eliza <94659901+elizacaws@users.noreply.github.com>
2021-12-07 20:26:04 -0800

    Adding forecast api

--- src/script.js
+++ src/script.js
@@ -21,7 +21,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -45,6 +46,14 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "6df09177bf10bb0a2b5eb9a9dc845bfd";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 //Display city, temp, and conditions
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -66,6 +75,8 @@ function displayTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -109,4 +120,3 @@ let celsiusElement = document.querySelector("#celsius");
 celsiusElement.addEventListener("click", displayCelsius);
 
 search("Los Angeles");
-displayForecast();
